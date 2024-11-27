cffd2b269467bbda68ae0181513324d0c9ba6e1a
Murilo <muriloazarias97@gmail.com>
2024-11-27 14:19:29 -0300

    first commit adding sipoc starting itens

--- src/App.tsx
+++ src/App.tsx
@@ -24,11 +24,84 @@ const EDGE_TYPES = {
   default: DefaultEdge,
 };
 
-const INITIAL_NODES: Node[] = [];
+const SIPOC_INITIAL_NODES = [
+  {
+    id: "suppliers",
+    type: "square",
+    position: { x: 100, y: 200 },
+    data: {
+      label: "Suppliers",
+      color: "#4FD1C5",
+    },
+  },
+  {
+    id: "inputs",
+    type: "square",
+    position: { x: 400, y: 200 },
+    data: {
+      label: "Inputs",
+      color: "#F6AD55",
+    },
+  },
+  {
+    id: "process",
+    type: "square",
+    position: { x: 700, y: 200 },
+    data: {
+      label: "Process",
+      color: "#90CDF4",
+    },
+  },
+  {
+    id: "outputs",
+    type: "square",
+    position: { x: 1000, y: 200 },
+    data: {
+      label: "Outputs",
+      color: "#FEB2B2",
+    },
+  },
+  {
+    id: "customers",
+    type: "square",
+    position: { x: 1300, y: 200 },
+    data: {
+      label: "Customers",
+      color: "#B794F4",
+    },
+  },
+];
+
+const SIPOC_INITIAL_EDGES = [
+  {
+    id: "suppliers-inputs",
+    source: "suppliers",
+    target: "inputs",
+    type: "default",
+  },
+  {
+    id: "inputs-process",
+    source: "inputs",
+    target: "process",
+    type: "default",
+  },
+  {
+    id: "process-outputs",
+    source: "process",
+    target: "outputs",
+    type: "default",
+  },
+  {
+    id: "outputs-customers",
+    source: "outputs",
+    target: "customers",
+    type: "default",
+  },
+];
 
 function App() {
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(SIPOC_INITIAL_EDGES);
+  const [nodes, setNodes, onNodesChange] = useNodesState(SIPOC_INITIAL_NODES);
 
   const onConnect = useCallback((connection: Connection) => {
     if (connection.source !== connection.target) {
